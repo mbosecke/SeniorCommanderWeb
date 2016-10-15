b7907a25afdb40d6dd4cbd874dedaef580760361
Mitchell Bösecke <mitchellbosecke@gmail.com>
2016-10-14 22:34:15 -0600

    More URL adjustments

--- src/main/java/com/mitchellbosecke/seniorcommander/web/controller/RootController.java
+++ src/main/java/com/mitchellbosecke/seniorcommander/web/controller/RootController.java
@@ -1,8 +1,15 @@
 package com.mitchellbosecke.seniorcommander.web.controller;
 
+import com.mitchellbosecke.seniorcommander.web.domain.CommunityUserModel;
+import com.mitchellbosecke.seniorcommander.web.service.CommunityUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Set;
 
 /**
  * Created by mitch_000 on 2016-09-11.
@@ -10,16 +17,23 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class RootController {
 
+    @Autowired
+    private CommunityUserService communityUserService;
+
     @RequestMapping("/")
     public String community() {
         return "redirect:/home";
     }
 
     @RequestMapping("/home")
-    public ModelAndView home() {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("home");
-        return mav;
+    public String home() {
+        if (!(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken)) {
+            // TODO: don't redirect to a random community, list out the available communities (if more than one)
+            Set<CommunityUserModel> communityUserModels = communityUserService.findMemberships();
+            return "redirect:/" + communityUserModels.iterator().next().getCommunityModel().getName();
+        }else{
+            return "home";
+        }
     }
 
     @RequestMapping("/privacy")
